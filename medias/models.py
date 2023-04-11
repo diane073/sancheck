a6af8f2c1f3a0a7a2c56533af05439861e3fa5a9
engulfedInFlames <recona97@gmail.com>
2023-04-11 17:28:25 +0900

    social login feature in progress

---
+++ medias/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Photo(models.Model):
+    pass
+
+
+# class Video(models.Model):
+#     pass
--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("user/signup/", views.signup, name="signup"),
+    path("user/signup", views.signup_view, name="signup"),
+    path("user/login", views.login_view, name="login"),
 ]
