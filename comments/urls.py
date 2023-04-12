d2100fa618c1330e134ad9529bb008fd2157abdd
diane073 <bear9473@gmail.com>
2023-04-12 13:22:05 +0900

    풀 후 충돌오류 수정,

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("/comment/note", views.comment_view, name="write_comments"),
-    # path("/comment/my", views., name="my_comments"),
+    path("/comment/my", views.my_comment_view, name="my_comments"),
 ]
