65f3e9ca3e3bea6992752c5258f87aa7113a29f3
diane073 <bear9473@gmail.com>
2023-04-14 16:30:24 +0900

    불필요한 주석 지우고 코드 정리

--- comments/views.py
+++ comments/views.py
@@ -4,19 +4,13 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import CommentModel
 from .forms import CommentForm
 from django.http import HttpResponse
-from django.core.paginator import Paginator
 from posts.models import PostModel
-
 from django.views import generic
-
-# Create your views here.
-# /post/<int:post_id>/detail < 글 상세보기 경로
 
 
 def comment_view(request):
-    if request.method == "GET":  # -> posts쪽에 병합하는것으로
+    if request.method == "GET":
         # 게시글 내용이 뜰때 같이 가져옴
-        # 여기 경로 이름 확인해야함
         all_comments = CommentModel()
         if not all_comments:
             message = "아직 댓글이 없습니다."
@@ -42,13 +36,9 @@ def comment_post(request, post_id):
             # 가능하다면 html에 {{error_message}} 띄워주기
             message = "댓글 내용을 적어주세요"
             return render(request, "post/post_detail.html", {"error_message": message})
-            # commnet/note의 html에 해당하는 것 넣어주기
 
         elif comment_form.is_valid():
             # 해당하는 포스트, 요청 User함께 입력하여 저장
-            # comment = CommentModel()
-            # comment.post = post
-            # comment.author = request.user
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
             new_comment.author = request.user
@@ -62,7 +52,7 @@ def comment_post(request, post_id):
 @login_required
 def comment_delete(request, post_id, comment_id):
     # comment_id가 일치하는 것을 가져와서 지우기
-    # post_id를 가져와 지운 창에 돌아가게 해준다.
+    # post_id를 가져와 댓글을 지운 창에 돌아가게 해준다.
     if request.method == "POST":
         comment = CommentModel.objects.get(id=comment_id)
         comment.delete()
@@ -70,6 +60,7 @@ def comment_delete(request, post_id, comment_id):
 
 
 class MyCommentList(generic.ListView):
+    # ListView를 사용해 댓글 10개씩 보여주기
     model = CommentModel
     template_name = "comments/my_comment.html"
     context_object_name = "latest_comment_list"
@@ -82,22 +73,3 @@ class MyCommentList(generic.ListView):
 
 
 get_my_comments = MyCommentList
-
-
-# def my_comment_view(request, page=1):
-#     if request.method == "GET":
-#         # 내 댓글 불러오기, author정보기반
-
-#         my_comment = CommentModel.objects.filter(author=request.user).order_by(
-#             "-created_at"
-#         )
-
-#         paginator = Paginator(my_comment, 10)
-#         page = 1
-#         page_obj = paginator.page(page)
-#         # return HttpResponse ('[%s]' % commnet_title)
-#         return render(
-#             None,
-#             "comments/my_comment.html",
-#             {"my_comment": my_comment, "page_obj": page_obj},
-#         )
